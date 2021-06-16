b0cf7a6091ecbbc8a9c01fae199171f3f553d793
Muhamad Irfan Fani <izzanrasyid31@gmail.com>
2021-06-16 13:31:57 +0700

    main content finished

--- src/components/RepoDetail.js
+++ src/components/RepoDetail.js
@@ -5,11 +5,11 @@ export default function RepoDetail (props) {
   return (
     <>
       <div class="card text-white bg-secondary my-2 mx-5">
-        <h5 class="card-header">{props.owner}</h5>
+        <h5 class="card-header Font-style-user">{props.name}</h5>
         <div class="card-body">
-          <h5 class="card-title">{props.name}</h5>
+          <h5 class="card-title Font-style-name">{props.owner}</h5>
           {
-            props?.lang ? <p class="card-text">language: {props.lang}</p>
+            props?.lang ? <p class="card-text Font-style-lang">language: {props.lang}</p>
             :
             null
           }
